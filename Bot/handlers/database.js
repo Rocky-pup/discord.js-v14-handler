51008a7d9669b29ac65e30ea54fe0f0d193c82ed
Rocky <83218418+Rocky-pup@users.noreply.github.com>
2022-11-12 21:45:06 +0000

    Update database.js

--- Bot/handlers/database.js
+++ Bot/handlers/database.js
@@ -6,19 +6,6 @@ const JoshMongo = require('@joshdb/mongo');
 module.exports = async (client) => {
     return new Promise(async (res) => {
 
-        const connectionOptions = {
-            useUnifiedTopology: true,
-            maxPoolSize: 100,
-            minPoolSize: 50,
-            writeConcern: "majority",
-        };
-         
-        // CACHE DURATION OPTIONS
-        process.env.DB_cache_ping = 10_000; // Delete the cache after X ms | < 0 === never delete [DEFAULT: 60_000]
-        process.env.DB_cache_get = 10_000; // Delete the cache after X ms | < 0 === never delete [DEFAULT: 300_000]
-        process.env.DB_cache_all = 10_000; // Delete the cache after X ms | < 0 === never delete [DEFAULT: 600_000]
-        // You can also add: db.get(key, true) // to force-fetch the db
-
         let dateNow = Date.now();
         client.logger("Now loading the Database ...")
         client.webhook(false, "Now loading the Database ...")
@@ -30,7 +17,7 @@ module.exports = async (client) => {
             providerOptions: {
               url: config.mongo,
               collection: "settings",
-              dbName: "test"
+              dbName: "rockys_handler"
             },
         });
         client.database.settings.defer.then( async () => {
